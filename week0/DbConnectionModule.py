420f2a86f4fa6f21ee04ae65ee98e94257cc6fa8
Bindeep <1lymebindeep@gmail.com>
2018-04-18 18:04:49 +0545

    Initizlized Connection

---
+++ week0/DbConnectionModule.py
@@ -0,0 +1,29 @@
+import sqlite3
+from sqlite3 import Error
+import logging
+
+
+class DbConnection:
+
+    """
+     Create a database connection to a database.
+    """
+
+    def __init__(self, db_name):
+
+        try:
+            self.connection_obj = sqlite3.connect(f'../Database/{db_name}')
+
+        except Error:
+            logging.error(f'Connection Unsuccessful with {Error}')
+
+    def get_cursor_obj(self):
+        self.cursor_obj = self.connection_obj.cursor()
+        return self.cursor_obj
+
+    def commit(self):
+        self.connection_obj.commit()
+
+    def close_connection(self):
+        if self.connection_obj:
+            self.connection_obj.close()
